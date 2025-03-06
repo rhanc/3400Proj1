e3c717554b6bced6cccb9f71c24f6cf70829e47e
rhanc <hancockr@etsu.edu>
2025-03-05 21:46:13 -0500

    server test

--- udp_server.py
+++ udp_server.py
@@ -1,13 +1,33 @@
-import socket
-
-HOST = ''
+import socket  
+HOST = '127.0.0.1'
 PORT = 5000
+ExpectedSeqValue = 0
+ActualSeqValue = 0
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def checkValue(ExpectedSeqValue,ActualSeqValue,socket,addr):
+    if ExpectedSeqValue == ActualSeqValue:
+        print("Expected value found")
+        print(f"Sending value: {ActualSeqValue}".encode())
+        socket.sendto(ActualSeqValue.encode(),addr)
+        ExpectedSeqValue = 1 - ExpectedSeqValue
+    elif ExpectedSeqValue != ActualSeqValue:
+        print("Expected value not found")
+        value = 1 - ExpectedSeqValue
+        socket.sendto(f"Resending the lask ACK: {value}".encode())
+
+def runServer():
+    data, addr = s.recvfrom(1024)
+    ActualSeqValue,text = data.decode().split(":",1)
+    value = int(ActualSeqValue)
+    ActualSeqValue = value
+    print(f"Received: '{data.decode()}' from {addr}")
+    #s.sendto(data, addr)
+    checkValue(ExpectedSeqValue,ActualSeqValue,socket,addr)
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s: 
     s.bind((HOST, PORT))
     print(f"Listening on port {PORT}")
 
     while True: 
-        data, addr = s.recvfrom(1024)
-        print(f"Received: '{data.decode()}' from {addr}")
-        s.sendto(data, addr)
+        runServer()
+    
