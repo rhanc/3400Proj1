aba3eba71e2d9098a9c0df00deab875c0e8e145c
rhanc <hancockr@etsu.edu>
2025-03-07 19:24:56 -0500

    server update

--- udp_client.py
+++ udp_client.py
@@ -1,46 +1,56 @@
-import socket as s 
-HOST = '127.0.0.1'  
-PORT = 5000
-expectedValue = 0
-message = "Hello World"
-socket = s.socket(s.AF_INET, s.SOCK_DGRAM) 
-address = (HOST,PORT)
-def CheckValues(seqActual, seqExpected):
-    result = False
-    if seqActual == seqExpected:
-        print("Correct ACK value: "+seqActual)
-        seqActual = 1 - seqActual
-        result = True
-    elif seqActual != seqExpected:
-        print("Incorrect value") 
-    return result
-def runClient(sValue,message,socket,address):
-    #data
-    #dataACK
-    #addr
-    #socket = 0
-    result = True
-    if result == True:
-        while result == True:
-            MessageResult = f"{sValue,message}"
-            socket.sendto(MessageResult.encode(),address)
-            try:
-                data, addr = socket.recvfrom(1024)
-                dataACK = data.decode().split(":")
-                dataACK = int(dataACK)
-                result = CheckValues(dataACK, sValue)
-                if result == True:
-                    break
-            except:
-                print("Request timedout")
-    socket.close()
+# import socket as s 
+# HOST = '127.0.0.1'  
+# PORT = 5000
+# expectedValue = 0
+# message = "Hello World"
+# socket = s.socket(s.AF_INET, s.SOCK_DGRAM) 
+# address = (HOST,PORT)
+# def CheckValues(seqActual, seqExpected):
+#     result = False
+#     if seqActual == seqExpected:
+#         print("Correct ACK value: "+seqActual)
+#         seqActual = 1 - seqActual
+#         result = True
+#     elif seqActual != seqExpected:
+#         print("Incorrect value") 
+#     return result
+# def runClient(sValue,message,socket,address):
+#     #data
+#     #dataACK
+#     #addr
+#     #socket = 0
+#     result = True
+#     if result == True:
+#         while result == True:
+#             MessageResult = f"{sValue,message}"
+#             socket.sendto(MessageResult.encode(),address)
+#             try:
+#                 data, addr = socket.recvfrom(1024)
+#                 dataACK = data.decode().split(":")
+#                 dataACK = int(dataACK)
+#                 result = CheckValues(dataACK, sValue)
+#                 if result == True:
+#                     break
+#             except:
+#                 print("Request timedout")
+#     socket.close()
 
 
-runClient(expectedValue,message,socket,address)
+# runClient(expectedValue,message,socket,address)
 
-# with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s: 
+# # with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s: 
     
-#     s.sendto(b'Hello, world!', (HOST, PORT))
-#     data, addr = s.recvfrom(1024)
+# #     s.sendto(b'Hello, world!', (HOST, PORT))
+# #     data, addr = s.recvfrom(1024)
 
-# print(f"Received '{data.decode()}' from {addr}")
+# # print(f"Received '{data.decode()}' from {addr}")
+
+import socket 
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+HOST = '127.0.0.1'
+PORT = 0
+addr = (HOST,PORT)
+data = "Hello"
+client.sendto(data.encode(),addr)
+print("Sent to server")
--- udp_server.py
+++ udp_server.py
@@ -1,38 +1,42 @@
-import socket  
+# import socket  
+# HOST = '127.0.0.1'
+# PORT = 5000
+# ExpectedSeqValue = 0
+# ActualSeqValue = 0
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s.bind((HOST, PORT))
+# def checkValue(ExpectedSeqValue,ActualSeqValue,socket,addr):
+#     if ExpectedSeqValue == ActualSeqValue:
+#         print("Expected value found")
+#         print(f"Sending value: {ActualSeqValue}".encode())
+#         socket.sendto(ActualSeqValue.encode(),addr)
+#         ExpectedSeqValue = 1 - ExpectedSeqValue
+#     elif ExpectedSeqValue != ActualSeqValue:
+#         print("Expected value not found")
+#         value = 1 - ExpectedSeqValue
+#         socket.sendto(f"Resending the lask ACK: {value}".encode())
+
+# def runServer():
+#     data, addr = s.recvfrom(1024)
+#     ActualSeqValue,text = data.decode().split(":",1)
+#     value = int(ActualSeqValue)
+#     ActualSeqValue = value
+#     print(f"Received: '{data.decode()}' from {addr}")
+#     #s.sendto(data, addr)
+#     checkValue(ExpectedSeqValue,ActualSeqValue,socket,addr)
+
+# print(f"Listening on port {PORT}")
+
+# while True: 
+#     runServer()
+ 
+
+import socket
 HOST = '127.0.0.1'
 PORT = 5000
-ExpectedSeqValue = 0
-ActualSeqValue = 0
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((HOST, PORT))
-def checkValue(ExpectedSeqValue,ActualSeqValue,socket,addr):
-    if ExpectedSeqValue == ActualSeqValue:
-        print("Expected value found")
-        print(f"Sending value: {ActualSeqValue}".encode())
-        socket.sendto(ActualSeqValue.encode(),addr)
-        ExpectedSeqValue = 1 - ExpectedSeqValue
-    elif ExpectedSeqValue != ActualSeqValue:
-        print("Expected value not found")
-        value = 1 - ExpectedSeqValue
-        socket.sendto(f"Resending the lask ACK: {value}".encode())
-
-def runServer():
-    data, addr = s.recvfrom(1024)
-    ActualSeqValue,text = data.decode().split(":",1)
-    value = int(ActualSeqValue)
-    ActualSeqValue = value
-    print(f"Received: '{data.decode()}' from {addr}")
-    #s.sendto(data, addr)
-    checkValue(ExpectedSeqValue,ActualSeqValue,socket,addr)
-
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind(HOST,PORT)
 print(f"Listening on port {PORT}")
-
-while True: 
-    runServer()
-# with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s: 
-   
-#     print(f"Listening on port {PORT}")
-
-#     while True: 
-#         runServer()
-    
+while True:
+   data, addr = server.recvfrom(1024)
+   print(f"Received: '{data.decode()}' from {addr}")
