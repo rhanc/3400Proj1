33feaecc6d08d2496233201f69cbd815188fe034
rhanc <hancockr@etsu.edu>
2025-03-07 19:26:16 -0500

    server update

--- udp_server.py
+++ udp_server.py
@@ -35,7 +35,7 @@ import socket
 HOST = '127.0.0.1'
 PORT = 5000
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(HOST,PORT)
+server.bind((HOST,PORT))
 print(f"Listening on port {PORT}")
 while True:
    data, addr = server.recvfrom(1024)
