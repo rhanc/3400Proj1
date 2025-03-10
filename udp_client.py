b07c120b218744de12f1e14182e3606ff8cd85ce
rhanc <hancockr@etsu.edu>
2025-03-10 19:12:14 -0400

    server test

--- udp_client.py
+++ udp_client.py
@@ -49,7 +49,7 @@ import socket
 def runClient(seqNum, text,data,h,p):
     count = 0
     HOST = '127.0.0.1'
-    PORT = 0
+    PORT = 5005
     addr = (HOST,PORT)
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
     client.bind((HOST,PORT))
@@ -59,7 +59,7 @@ def runClient(seqNum, text,data,h,p):
         text = f"Server Data: "+str(seqNum)
         #data = struct.pack("!I",seqNum)+text.encode()
         data = text.encode()
-        client.sendto(data,addr)
+        client.sendto(data,(HOST,PORT))
         print("Complete. Sequence Number: "+str(seqNum))
         count = count + 1
     client.close()
