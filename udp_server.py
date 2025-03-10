e1a5ca3561971fba57e1951151f460d6fce96e41
rhanc <hancockr@etsu.edu>
2025-03-10 19:21:10 -0400

    server test

--- udp_server.py
+++ udp_server.py
@@ -1,40 +1,15 @@
-# import socket  
-# HOST = '127.0.0.1'
-# PORT = 5000
-# ExpectedSeqValue = 0
-# ActualSeqValue = 0
-# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# s.bind((HOST, PORT))
-# def checkValue(ExpectedSeqValue,ActualSeqValue,socket,addr):
-#     if ExpectedSeqValue == ActualSeqValue:
-#         print("Expected value found")
-#         print(f"Sending value: {ActualSeqValue}".encode())
-#         socket.sendto(ActualSeqValue.encode(),addr)
-#         ExpectedSeqValue = 1 - ExpectedSeqValue
-#     elif ExpectedSeqValue != ActualSeqValue:
-#         print("Expected value not found")
-#         value = 1 - ExpectedSeqValue
-#         socket.sendto(f"Resending the lask ACK: {value}".encode())
 
-# def runServer():
-#     data, addr = s.recvfrom(1024)
-#     ActualSeqValue,text = data.decode().split(":",1)
-#     value = int(ActualSeqValue)
-#     ActualSeqValue = value
-#     print(f"Received: '{data.decode()}' from {addr}")
-#     #s.sendto(data, addr)
-#     checkValue(ExpectedSeqValue,ActualSeqValue,socket,addr)
-
-# print(f"Listening on port {PORT}")
-
-# while True: 
-#     runServer()
  
 
 import socket
 import struct
-def bindServer(s):
-   server.bind((HOST,PORT))
+HOST = '127.0.0.1'
+PORT = 5005
+data = ""
+addr = "" 
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+def bindServer(s,HOST,PORT):
+   s.bind((HOST,PORT))
 def printMessage(message):
    print(message)
 def showResult(addr, ActualSeqValue,nums):
@@ -49,18 +24,8 @@ def runServer(data,addr,server):
     if result == True:
        continue
     ActualSeqValue,nums = struct.unpack("!I", data[:4])[0],data[4:]
-    showResult(addr, ActualSeqValue,nums)
-    #print(f"Received: '{data.decode()}' from {addr}")
-HOST = '127.0.0.1'
-PORT = 5005
-data = ""
-addr = "" 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# server.bind((HOST,PORT))
-bindServer(server)
-# print(f"Listening on port {PORT}")
+    showResult(addr, ActualSeqValue,nums) 
+
+bindServer(server,HOST,PORT) 
 printMessage(f"Listening on port {PORT}")
-runServer(data,addr,server)
-# while True:
-#    data, addr = server.recvfrom(1024)
-#    print(f"Received: '{data.decode()}' from {addr}")
+runServer(data,addr,server) 
